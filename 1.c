661389d3a5103f80ae7b4095bce1728b86f43bbe
Groverkss <groverkss@gmail.com>
2021-03-21 19:30:35 +0530

    Initial commit

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <sys/types.h>
+
+int32_t main(void) {
+    printf("Hello World!\n"); 
+}
---
+++ sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -au /home/kunwar/Desktop/Projects/SPP1 abacus:~/
+rsync -au abacus:~/SPP1 /home/kunwar/Desktop/Projects/ 
